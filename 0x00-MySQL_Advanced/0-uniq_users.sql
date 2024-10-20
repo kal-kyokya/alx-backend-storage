cbb6fad5773fc32489e2035983e56bd14d4e934d
kal-kyokya <kalkyokya4@gmail.com>
2024-10-20 12:02:06 +0000

    Write a script creating a table named 'users'.

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -4,7 +4,7 @@
 -- before attempting table creation
 
 CREATE TABLE IF NOT EXISTS users (
-   id INT AUTO_INCREMENT PRIMARY KEY,
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    email VARCHAR(255) UNIQUE NOT NULL,
    name VARCHAR(255)
 );
