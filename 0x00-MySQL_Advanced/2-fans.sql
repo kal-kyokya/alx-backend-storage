fe83a7db6a53bce02b1ece9ef7f47f4a1fb4ab63
kal-kyokya <kalkyokya4@gmail.com>
2024-10-20 13:15:25 +0000

    Write an sql script selecting and sorting all elements of a table.

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
 -- Select all elements of a table
 -- after sorting them with 'order by'
 
-SELECT origin, fans as nb_fans FROM metal_bands
-ORDER BY nb_fans;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
