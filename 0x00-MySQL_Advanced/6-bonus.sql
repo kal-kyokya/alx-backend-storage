7a443c1ec42917555007dcbd4ef9f8997c2d0700
kal-kyokya <kalkyokya4@gmail.com>
2024-10-21 11:35:05 +0000

    Write an sql script creating a procedure updating 2 database tables.

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,27 @@
+-- Create a 'stored procedure' named AddBonus
+-- that insert values inside a designated table
+
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus(
+    IN userId INT,
+    IN projectName VARCHAR(255),
+    IN userScore INT
+)
+
+BEGIN
+    -- Insert the project if it doesn't exist
+    INSERT INTO projects (name)
+    SELECT projectName
+    WHERE NOT EXISTS (SELECT * FROM projects WHERE name = projectName LIMIT 1);
+
+    -- Insert the input data into the intended table
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (
+        userId,
+        COALESCE(SELECT id FROM projects WHERE name = projectName, LAST_INSERT_ID()),
+        userScore
+    );
+
+END $$
+DELIMITER;
