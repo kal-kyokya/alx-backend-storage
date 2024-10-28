286ce5e19c2dd46b9cd3659d543251444569bd0f
kal-kyokya <kalkyokya4@gmail.com>
2024-10-28 14:15:00 +0000

    Upgrade a class enabling Redis database manipulation.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 'exercise' creates a class enabling manipulation of a Redis database
 """
 import redis
-from typing import Union
+from typing import Callable, Optional, Union
 import uuid
 
 
@@ -21,3 +21,30 @@ class Cache:
         self._redis.set(key, data)
 
         return (key)
+
+    def get(
+            self,
+            key: str,
+            fn: Optional[Callable] = None,
+    ) -> Union[str, bytes, int, float, None]:
+        """
+        returns the value stored in the redis store at the key by converting it
+        to its original data type by calling the function fn. if the key is not
+        found, it returns None
+        """
+        value = self._redis.get(key)
+        if value is not None and fn is not None:
+            value = fn(value)
+        return value
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        returns the value stored in the redis store at the key as an int
+        """
+        return self.get(key, int)  # type: ignore
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        returns the value stored in the reds store at the key as str
+        """
+        return self.get(key, str)  # type: ignore
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -15,3 +15,20 @@ print(key)
 local_redis = redis.Redis()
 print(local_redis.get(key))
 
+del cache
+del key
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    "bar": lambda d: d.decode("utf-8"),
+    123: int,
+    b"foofoo": lambda d: d.decode("utf-8"),
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    print(cache.get(key, fn=fn), value)
+    print(type(cache.get(key, fn=fn)))
+    assert cache.get(key, fn=fn) == value
