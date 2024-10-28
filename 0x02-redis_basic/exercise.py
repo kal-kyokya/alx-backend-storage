cdb6ad62e4b0f8fe597941fc9ce387b7d4786881
kal-kyokya <kalkyokya4@gmail.com>
2024-10-28 06:40:28 -0700

    Create a class enabling Redis database manipulation.

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+'exercise' creates a class enabling manipulation of a Redis database
+"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """Blueprint facilitating Redis database interaction."""
+
+    def __init__(self):
+        """Constructor for all class instances."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[bytes, float, int, str]) -> str:
+        """Write information in the Redis database."""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+
+        return (key)
