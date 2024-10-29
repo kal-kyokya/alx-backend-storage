8fe0d98d56444ccdcd33dbd486ffe9a6f676030b
kal-kyokya <kalkyokya4@gmail.com>
2024-10-29 11:31:41 +0000

    Create a wrapper function generating a report on a Redis database.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""Redis basic"""
 import redis
 import requests
 from functools import wraps
@@ -8,17 +7,14 @@ from typing import Callable
 
 
 redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+"""The module -level Redis instance"""
 
 
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+    """Caches the output of fetched data"""
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
+    def invoker(url: str) -> str:
+        """The wrapper function for caching the output"""
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
@@ -32,7 +28,5 @@ def data_cacher(method: Callable) -> Callable:
 
 @data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
+    """Implements an expiring web cache and tracker"""
     return requests.get(url).text
